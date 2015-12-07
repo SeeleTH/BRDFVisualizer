0dea2bf22b23e0ebbd67f0fa23c188a17c108548
SeeleTH <namo_95@hotmail.com>
2015-12-07 16:56:50 +0900

    change gl version to 4.3

--- src/glhelper.cpp
+++ src/glhelper.cpp
@@ -609,7 +609,7 @@ namespace NPGLHelper
 			return 0;
 
 		glfwInit();
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
